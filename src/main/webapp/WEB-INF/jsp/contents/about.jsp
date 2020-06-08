11f636d48e6750285e7488001e23fa150558a7b2
Grzesiek Kaczor <grzechu.kaczor@gmail.com>
2020-06-08 21:48:56 +0200

    Added some describe about app

--- src/main/webapp/WEB-INF/jsp/contents/about.jsp
+++ src/main/webapp/WEB-INF/jsp/contents/about.jsp
@@ -1,5 +1,6 @@
-<article>
-    O aplikacji :)
+<article class="text-center">
+    <h3 class="mt-4">Blue Soft Contract Application</h3>
+    <p>Jest to aplikacja stworzona na potrzeby rekrutacyjne. Służy do zbierania danych o umowach zawartych przez firmę BlueSoft</p>
 
 
 </article>
---
+++ src/main/webapp/resources/script/aboutScript.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    var table = $('#contract-tab').DataTable({
+        info: false,
+        pagingType: "simple_numbers"
+    });
+    $.getJSON('http://localhost:8080/contract/active', function (data) {
+        data.forEach(function (dt) {
+                const orderNumber = dt.orderNumber;
+                const system = dt.system;
+
+                const fromDate = dt.fromDate.year + "-" + dt.fromDate.month + "- " + dt.fromDate.day;
+                const toDate = dt.toDate.year + "-" + dt.toDate.month + "-" + dt.toDate.day;
+
+                var valueMapAmountType;
+                if (dt.amountType === "NET") {
+                    valueMapAmountType = "netto";
+                } else {
+                    valueMapAmountType = "brutto";
+                }
+                ;
+                const amount = dt.amount / 100 + " " + valueMapAmountType;
+
+                var valueMapActivePeriod;
+
+                if (dt.amountPeriod === "MONTH") {
+                    valueMapActivePeriod = "na miesiąc";
+                } else {
+                    valueMapActivePeriod = "na rok";
+                }
+                ;
+                const amountPeriod = valueMapActivePeriod;
+
+                var activeMapValue;
+                if (dt.active === true) {
+                    activeMapValue = "TAK";
+                } else {
+                    activeMapValue = "NIE";
+                }
+                ;
+                const active = activeMapValue;
+
+                data = [orderNumber, system, fromDate, toDate, amount, amountPeriod, active];
+                table.row.add(data).draw();
+            }
+        );
+    });
+});
+
+
+
